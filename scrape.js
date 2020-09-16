ac18fb2865b35f0a980f77cd603c9ca039b7eb1e
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-09-15 20:28:33 -0700

    fixed issues with scraper not stopping

--- scrape.js
+++ scrape.js
@@ -1,9 +1,11 @@
 const Request = require('./lib/Request');
 const parse = require('./lib/parse');
 const store = require('./lib/store');
+const pool = require('./lib/utils/pool');
 
-setInterval(() => {
-  Request()
-    .then(document => parse(document))
-    .then(books => store(books));
-});
+
+Request()
+  .then(document => parse(document))
+  .then(books => store(books))
+  .finally(() => pool.end());
+
