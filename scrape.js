176be4f4cbecbad6c0128b7d1c8989d541e1e0a2
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-09-18 16:58:20 -0700

    merging issues with scrape.js

--- scrape.js
+++ scrape.js
@@ -1,9 +1,11 @@
 const Request = require('./lib/Request');
 const parse = require('./lib/parse');
 const store = require('./lib/store');
+const pool = require('./lib/utils/pool');
 
 
 Request()
   .then(document => parse(document))
-  .then(books => store(books));
+  .then(books => store(books))
+  .finally(() => pool.end());
 
